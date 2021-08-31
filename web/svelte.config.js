fb3945becabc75b3fba133779f7a55c4639e6c79
Cryptogenik <70092290+CRYPTO-GENIK@users.noreply.github.com>
2021-08-31 10:19:19 -0500

    Initialize sveltekit-bug-default-import

---
+++ web/svelte.config.js
@@ -0,0 +1,15 @@
+import preprocess from 'svelte-preprocess';
+import staticAdapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: preprocess(),
+
+  kit: {
+    adapter: staticAdapter(),
+    target: '#svelte',
+    trailingSlash: 'ignore',
+  },
+};
+
+export default config;
